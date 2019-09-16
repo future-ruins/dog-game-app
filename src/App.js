edc2254582a12d2b941ea43c01c32ef3e3b09c33
future-ruins <helliot885@gmail.com>
2019-09-16 17:27:46 +0200

    Setup fixes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from "react";
 import "./App.css";
 import { connect } from "react-redux";
+import { Route } from "react-router-dom";
 
 class App extends React.Component {
   render() {
-    return <div className="App"></div>;
+    return (
+      <div className="App">
+        <main>
+          <Route exact path="/" component={DogsListContainer} />
+          {/* :breed is a parameter!! IF THIS PARAMETER IS AT THE END OF
+      THE PATH SHOW THIS COMPONENT */}
+          <Route
+            path="/dog-breeds/:breed"
+            component={DogBreedImagesContainer}
+          />
+        </main>
+      </div>
+    );
   }
 }
 
