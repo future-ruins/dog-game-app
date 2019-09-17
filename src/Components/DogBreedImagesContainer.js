91bd62b3952ff4ee0d5f7307f5f89c78b8d47e8f
future-ruins <helliot885@gmail.com>
2019-09-17 11:45:42 +0200

    Display 10 images per page with local state

--- src/Components/DogBreedImagesContainer.js
+++ src/Components/DogBreedImagesContainer.js
@@ -1,26 +1,37 @@
-import React, {Component} from 'react'
-import DogBreedImages from './DogBreedImages'
-import request from 'superagent'
+import React, { Component } from "react";
+import DogBreedImages from "./DogBreedImages";
+import request from "superagent";
+import { connect } from "react-redux";
 
-export default class DogBreedImagesContainer extends Component {
-  state = { images: null }
+class DogBreedImagesContainer extends Component {
+  state = { images: null };
 
   componentDidMount() {
-    const breed = this.props.match.params.breed
+    const breed = this.props.match.params.breed;
     request
       .get(`https://dog.ceo/api/breed/${encodeURIComponent(breed)}/images`)
       .then(response => this.updateImages(response.body.message))
-      .catch(console.error)
+      .catch(console.error);
   }
 
   updateImages(images) {
+    const tenImages = images.slice(0, 10);
     this.setState({
-      images: images
-    })
+      images: tenImages
+    });
   }
 
   render() {
-    console.log('Container props test:', this.props)
-    return <DogBreedImages match={this.props.match} images={ this.state.images } />
+    console.log("Container props test:", this.props);
+    return (
+      <DogBreedImages match={this.props.match} images={this.state.images} />
+    );
   }
 }
+
+const mapStateToProps = state => {
+  // console.log("STATE OF REDUX STORE", state);
+  return { state: state };
+};
+
+export default connect(mapStateToProps)(DogBreedImagesContainer);
