7f6da3389bdf81fc1732e65980792b6557d571f0
eawh02 <estherhayward@gmail.com>
2019-09-19 21:23:38 +0200

    link homepage and add welcome message

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -4,6 +4,15 @@ export default class Homepage extends React.Component {
   render() {
     return (
       <div className="welcomeImage">
+        <h2>Welcome! Are you ready to have fun with dogs?
+          <br></br>
+          <br></br>
+            Check out our dogs list. Here we list all the
+          <br></br>
+            dog breeds in the world! Do you know them all?
+          <br></br>
+          <br></br>
+            Play the games to test your knowledge!</h2>
         <img
           src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/8077ed19827641.562e0efbbaf83.jpg"
           alt="dogs illustration"
